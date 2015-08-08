a2ead42ed2b64903c67394de1264b8ef909dc314
klozz (TeamMEX) <xXx.Reptar.Rawrr.xXx@gmail.com>
2015-08-07 19:20:29 -0500

    change the url of web page due to cyan dark not allowed free web hosting anymore

--- src/com/xperience/center/AboutXPerience.java
+++ src/com/xperience/center/AboutXPerience.java
@@ -84,7 +84,7 @@ public class AboutXPerience extends Fragment{
         @Override
         public void onClick(View v) {
             if (v == website) {
-                launchUrl("http://xpe.cynd.gq/");
+                launchUrl("http://xpe.esy.es/");
             } else if (v == source) {
                 launchUrl("http://github.com/XPerience-AOSP-Lollipop");
             } else if (v == donate) {
